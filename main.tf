c82f8f6c641f204fa78a746396d32bb7d98aaf6f
Erik R. Rygg <errygg@gmail.com>
2020-05-08 14:14:54 -0600

    Update vault module version

--- main.tf
+++ main.tf
@@ -1,7 +1,19 @@
 provider "aws" {
   region = "${var.region}"
 }
+
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "erik-rygg"
+
+    workspaces {
+      name = "tfe-workspace-example"
+    }
+  }
+}
+
 module "vault" {
   source  = "app.terraform.io/erik-rygg/vault/aws"
-  version = "0.1.1"
+  version = "1.0.0"
 }
